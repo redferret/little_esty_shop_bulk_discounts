6465cd624150f38dcb2d1261fe2b3f66fc3a059b
Richard <rdesilvey@gmail.com>
2021-06-10 17:19:02 -0600

    Add expected feature tests

--- spec/features/merchants/bulk_discounts/index_spec.rb
+++ spec/features/merchants/bulk_discounts/index_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'The index page for merchants bulk discounts,' do
-  
+  describe 'discounts list,' do
+    it 'shows the discounts with their attributes'
+    it 'has a link to that discount show page'
+  end
 end
