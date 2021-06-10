f1064e05c7a041b8082925e59e39dc9b0a163a6b
Richard <rdesilvey@gmail.com>
2021-06-10 17:17:07 -0600

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
-  resources :bulk_discounts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   scope module: :merchants do
-    resources :merchants, only: [:show] do
-      resources :dashboard, only: [:index]
-      resources :items, except: [:destroy]
-      resources :item_status, only: [:update]
-      resources :invoices, only: [:index, :show, :update]
+    resources :merchants, only: [ :show  ] do
+      resources :dashboard, only: [ :index ]
+      resources :items, except: [ :destroy ]
+      resources :item_status, only: [ :update ]
+      resources :invoices, only: [ :index, :show, :update ]
+      resources :bulk_discounts, only: [ :index ]
     end
   end
 
   namespace :admin do
-    resources :dashboard, only: [:index]
-    resources :merchants, except: [:destroy]
-    resources :merchant_status, only: [:update]
-    resources :invoices, except: [:new, :destroy]
+    resources :dashboard, only: [ :index ]
+    resources :merchants, except: [ :destroy ]
+    resources :merchant_status, only: [ :update ]
+    resources :invoices, except: [ :new, :destroy ]
   end
 end
