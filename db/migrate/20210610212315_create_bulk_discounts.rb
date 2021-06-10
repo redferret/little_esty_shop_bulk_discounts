ab9b3ef19be96006f623c43860ffd33ad7d5f606
Richard <rdesilvey@gmail.com>
2021-06-10 15:42:17 -0600

    Update migration and schema

--- db/migrate/20210610212315_create_bulk_discounts.rb
+++ db/migrate/20210610212315_create_bulk_discounts.rb
@@ -1,8 +1,9 @@
 class CreateBulkDiscounts < ActiveRecord::Migration[5.2]
   def change
     create_table :bulk_discounts do |t|
-      t.percentage_discount :float
-      t.quantity_threshold :integer
+      t.float :percentage_discount
+      t.integer :quantity_threshold
+      t.references :merchant, foreign_key: true
 
       t.timestamps
     end
