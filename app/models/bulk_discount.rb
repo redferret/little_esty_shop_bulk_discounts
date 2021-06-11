b6c1f2e6974dcb91620af84a5ea8b03e05dbb9f1
Richard <rdesilvey@gmail.com>
2021-06-10 23:04:27 -0600

    Implement validations

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,5 +1,5 @@
 class BulkDiscount < ApplicationRecord
-  validates_presence_of :percentage_discount
+  validates_presence_of :percentage_discount, numericality: true
   validates_presence_of :quantity_threshold
 
   belongs_to :merchant
