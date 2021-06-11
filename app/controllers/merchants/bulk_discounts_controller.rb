837629d362e22ffe293c389f25966473c2ee6109
Richard <rdesilvey@gmail.com>
2021-06-10 23:06:32 -0600

    Implement crud in controller

--- app/controllers/merchants/bulk_discounts_controller.rb
+++ app/controllers/merchants/bulk_discounts_controller.rb
@@ -8,9 +8,46 @@ class Merchants::BulkDiscountsController < ApplicationController
   def show
     @bulk_discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    if @merchant.bulk_discounts.create!(bulk_discount_params)
+      redirect_to merchant_bulk_discounts_path(@merchant)
+    else
+      flash[:alert] = "Couldn't create discount"
+      redirect_to edit_merchant_bulk_discount_path(@merchant, @bulk_discount)
+    end
+  end
+
+  def update
+    @bulk_discount = BulkDiscount.find(params[:id])
+
+    if @bulk_discount.update!(bulk_discount_params)
+      redirect_to merchant_bulk_discount_path(@merchant, @bulk_discount)
+    else
+      flash[:alert] = "Couldn't update discount"
+      redirect_to edit_merchant_bulk_discount_path(@merchant, @bulk_discount)
+    end
+  end
+
+  def destroy
+    bulk_discount = BulkDiscount.find(params[:id])
+    if bulk_discount.destroy!
+      redirect_to merchant_bulk_discount_path(@merchant, @bulk_discount)
+    else
+      flash[:alert] = "Couldn't delete discount"
+      redirect_to edit_merchant_bulk_discount_path(@merchant, @bulk_discount)
+    end
+  end
   
   private
-  def set_merchant_discount_models
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:percentage_discount, :quantity_threshold)
+  end
+
+  def set_merchant
     @merchant = Merchant.find(params[:merchant_id])
   end
 end
