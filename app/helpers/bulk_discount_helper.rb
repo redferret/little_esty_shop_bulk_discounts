6edda33c708e6412975050aa09527cf3b9fd5d57
Richard <rdesilvey@gmail.com>
2021-06-11 01:04:09 -0600

    Remove helper

--- app/helpers/bulk_discount_helper.rb
+++ app/helpers/bulk_discount_helper.rb
@@ -1,8 +1,4 @@
 module BulkDiscountHelper
-  def link_string(index, discount)
-    "#{index + 1}. Discount Offered: #{formatted_percentage_discount(discount)}, Item Threshold: #{discount.quantity_threshold}"
-  end
-
   def formatted_percentage_discount(discount)
     "%0.0f%%" % [discount.percentage_discount * 100]
   end
