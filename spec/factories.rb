50ed01e1e38e549f340abcc8066428925292cce5
Richard <rdesilvey@gmail.com>
2021-06-10 18:21:15 -0600

    Fix factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -11,32 +11,24 @@ FactoryBot.define do
 
   factory :invoice do
     status { [ 0, 1, 2 ].sample }
-    merchant
-    customer
   end
 
   factory :merchant do
     name { Faker::Space.galaxy }
-    invoices
-    items
   end
 
   factory :item do
     name { Faker::Coffee.variety }
     description { Faker::Hipster.sentence }
     unit_price { Faker::Number.decimal(l_digits: 2) }
-    merchant
   end
 
   factory :transaction do
     result { [ 0, 1 ].sample }
     credit_card_number {Faker::Finance.credit_card}
-    invoice
   end
 
   factory :invoice_item do
     status { [ 0, 1, 2 ].sample }
-    merchant
-    invoice
   end
 end
