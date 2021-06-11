4342d9c914a698813648765051fe5c5f543210bc
Richard <rdesilvey@gmail.com>
2021-06-10 21:23:43 -0600

    Implement API

--- app/services/nager_api/api_calls.rb
+++ app/services/nager_api/api_calls.rb
@@ -1,3 +1,15 @@
 module ApiCalls
+
+  def current_time
+    Time.now
+  end
   
+  def upcoming_holidays
+    year = Time.now.year
+    holidays = request endpoint: "/api/v3/PublicHolidays/#{year}/US"
+    
+    holidays.filter_map do |holiday|
+      holiday if current_time < Time.parse(holiday['date'])
+    end[0..2]
+  end
 end
