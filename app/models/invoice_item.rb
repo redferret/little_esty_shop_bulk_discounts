087fb953af05af75a71e7bc9c3eaae09c4546a73
Richard <rdesilvey@gmail.com>
2021-06-14 15:35:55 -0600

    Implement model method

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -11,6 +11,10 @@ class InvoiceItem < ApplicationRecord
 
   enum status: { pending: 0, packaged: 1, shipped: 2 }
 
+  def has_discount?
+    bulk_discount != nil
+  end
+
   def self.incomplete_invoices
     invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
     Invoice.order(created_at: :asc).find(invoice_ids)
