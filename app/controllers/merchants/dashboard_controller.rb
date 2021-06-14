4e4267b498f4801d5c390ed7601e8020b426b1e9
Richard <rdesilvey@gmail.com>
2021-06-14 15:07:39 -0600

    Reimplement dashboard controller
    To fix previous bugs from fresh fork

--- app/controllers/merchants/dashboard_controller.rb
+++ app/controllers/merchants/dashboard_controller.rb
@@ -1,5 +1,6 @@
 class Merchants::DashboardController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
+    @invoices_ready_to_ship = Invoice.invoices_ready_to_ship(@merchant.id)
   end
 end
