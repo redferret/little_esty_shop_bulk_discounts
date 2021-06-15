72d2f5665ff3f953950333fbbc2e957b37b6096d
Richard <rdesilvey@gmail.com>
2021-06-15 12:57:14 -0600

    Implement features

--- app/helpers/invoice_item_helper.rb
+++ app/helpers/invoice_item_helper.rb
@@ -11,7 +11,7 @@ module InvoiceItemHelper
 
   def calculate_total_revenue_with_discount(invoice_item)
     discount = 1 - (invoice_item.bulk_discount.percentage_discount / 100.0)
-    total_price = (invoice_item.unit_price) * invoice_item.quantity
-    number_to_currency(total_price / 100.0)
+    total_revenue = (invoice_item.unit_price) * invoice_item.quantity
+    number_to_currency((total_revenue * discount) / 100.0)
   end
 end
