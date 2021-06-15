5adc8fa0c136bc4578d3ce846858be0c83ba04e6
Richard <rdesilvey@gmail.com>
2021-06-15 13:06:58 -0600

    Simplify API call

--- app/services/nager_api/api_calls.rb
+++ app/services/nager_api/api_calls.rb
@@ -1,19 +1,6 @@
 module ApiCalls
-
-  def current_date
-    Time.now
-  end
-
-  def date_of_the(holiday)
-    Time.parse(holiday['date'])
-  end
-  
   def upcoming_holidays
-    year = Time.now.year
-    holidays = request endpoint: "/api/v3/PublicHolidays/#{year}/US"
-    
-    holidays.find_all do |holiday|
-      current_date < (date_of_the holiday)
-    end[0..2]
+    holidays = request endpoint: "/api/v3/NextPublicHolidays/US"
+    holidays[0..2]
   end
 end
